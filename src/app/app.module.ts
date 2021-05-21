77596cd8b503f05230e01ca186291d1ad33e41bf
Sanyam_Shaw <sanyam.tech.26@gmail.com>
2021-05-21 16:24:43 +0530

    create-post edit-profile ticket implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TicketComponent } from './ticket/ticket.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketComponent,
+    CreatePostComponent,
+    EditProfileComponent
   ],
   imports: [
     BrowserModule,
