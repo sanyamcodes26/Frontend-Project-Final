c14cbcca428f4dcf307b4b322f6057215d6bb77c
Sanyam_Shaw <sanyam.tech.26@gmail.com>
2021-06-06 21:47:44 +0530

    Dynamic dropdown added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { by, element } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Demo';
+  toggleOn = true;
+  toggleOn1 = true;
 }
+
+// it('should check ngHide', function() {
+//   var checkbox = element(by.model('checked'));
+//   var checkElem = element(by.css('.check-element'));
+//   var val = true;
+//   expect(checkElem.isDisplayed()).toBe(val);
+//   checkbox.click();
+//   expect(checkElem.isDisplayed()).toBe(val);
+// });
+
+
--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-post.component.css']
 })
 export class CreatePostComponent implements OnInit {
-
+  assignment = true;
+  lecture =true;
+  assignment_box = true;
+  lecture_box = true;
   constructor() { }
 
   ngOnInit(): void {
